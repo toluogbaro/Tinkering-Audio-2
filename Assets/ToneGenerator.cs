6bd92b09d329c234739b4766fa3a6b4ad84d5cd0
Tolu Ogbaro <toluogbaro123@gmail.com>
2019-12-02 18:21:54 +0000

    Added correct script

--- Assets/ToneGenerator.cs
+++ Assets/ToneGenerator.cs
@@ -14,10 +14,17 @@ public class ToneGenerator : MonoBehaviour
     //Establishing the max number of samples
     private int sampleFrequecy = 44100;
 
-    private float[] notes = { 523.25f, 391.995f };
+    private float[] notes = { 523.25f, 391.995f, 600.25f, 456.5f, 1300.05f };
     private int currentNote;
     [SerializeField]
-    private int[] sequence = { 0, 0, 1, 1, 0 };
+    public int[] sequence = { 5, 3, 1, 1, 2, 0 };
+    public int[] sequence2 = { 0, 1, 2, 3, 4, 5 };
+
+    public float[] frequencies;
+    public int thisfreq;
+    public double frequency = 440.0;
+
+
     //sequence referencing the position of the numbers in the array
 
     [SerializeField]
@@ -38,10 +45,25 @@ public class ToneGenerator : MonoBehaviour
     void Start()
     {
         PlayMelody();
+        frequencies = new float[8];
+        frequencies[0] = 440.0f;
+        frequencies[1] = 494.0f;
+        frequencies[2] = 554.0f;
+        frequencies[3] = 587.0f;
+        frequencies[4] = 659.0f;
+        frequencies[5] = 740.0f;
+        frequencies[6] = 831.0f;
+        frequencies[7] = 880.0f;
+
+        frequency = frequencies[thisfreq];
+        thisfreq += 1;
+
+
+
     }
 
     void CreateTone(float note)
-        
+
     {
         for (int i = 0; i < samples.Length; i++)
         {
@@ -53,19 +75,58 @@ public class ToneGenerator : MonoBehaviour
         clip.SetData(samples, 0);
     }
 
-    void PlayMelody()
+    void CreateTone2(float note)
     {
-        if (currentNote < sequence.Length)
-            currentNote += 1;
-        else
-            currentNote = 0;
+        for (int i = 0; i < samples.Length; i++)
+        {
+            samples[i] = Mathf.Cos(Mathf.PI * 3 * i * note / sampleFrequecy);
+
+        }
+
+        clip = AudioClip.Create("Note", samples.Length, 1, sampleFrequecy, false);
+        clip.SetData(samples, 0);
+
+        //the second note
+
+
+        //Creating own methods then referencing it in void start
+        //Create an array for the notes, Create a tempo; create sounds at certain intervals. Coroutine/invokerepeating to have a beat. Synthesise tones
+    }
+    void CreateTone3(float note)
+    {
+        for(int thisfreq = 1; thisfreq <= frequencies.Length; thisfreq++)
+        {
+            samples[thisfreq] = thisfreq / sampleLengthInSeconds;
+        }
+    }
+
+    void CreateTone4(float note)
+    {
+        frequency = 440.0 * 2.0 * (1760 / 12);
+    }
+
+
+    void PlayMelody()
+        {
+            if (currentNote < sequence.Length)
+                currentNote += 1;
+            //currentNote = currentNote + 1
+            else
+                currentNote = 0;
+
 
         CreateTone(notes[sequence[currentNote]]);
         //Connecting all this syntax
 
-        source.clip = clip;
-        source.Play();
-    }
-    //Creating own methods then referencing it in void start
-    //Create an array for the notes, Create a tempo; create sounds at certain intervals. Coroutine/invokerepeating to have a beat. Synthesise tones
+        CreateTone2(notes[sequence[currentNote]]);
+
+        CreateTone3(notes[sequence[currentNote]]);
+
+        CreateTone4(notes[sequence[currentNote]]);
+
+
+            source.clip = clip;
+            source.Play();
+
+        }
 }
